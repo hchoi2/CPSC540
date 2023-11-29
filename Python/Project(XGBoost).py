7aac9c043a5e7a3112dd0f0cdd27492424d8555f
Hyejeong Choi <hchoi2@student.ubc.ca>
2023-11-28 20:14:41 -0800

    Initial

--- Python/Project(XGBoost).py
+++ Python/Project(XGBoost).py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import AdaBoostClassifier as ABC
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 
 # Load data
 data = pd.read_csv('/Users/hyejeongchoi/Desktop/hchoi_homepage/hchoi2/Python/Data/WineQT.csv')
