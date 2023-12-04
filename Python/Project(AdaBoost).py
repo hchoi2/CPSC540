6707a27db90a71561cd2c6d4c473e2328ad61a36
Hyejeong Choi <hchoi2@student.ubc.ca>
2023-12-04 14:50:08 -0800

    Initial push

--- Python/Project(AdaBoost).py
+++ Python/Project(AdaBoost).py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.ensemble import AdaBoostClassifier as ABC
+from sklearn.ensemble import AdaBoostClassifier as abc
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+
+from sklearn.ensemble import RandomForestClassifier
 
 # Load data
 data = pd.read_csv('./Data/WineQT.csv')
@@ -13,3 +15,38 @@ print(y.head())
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+####
+# Convert the data to LightGBM dataset format
+train_data = abc(X_train, label=y_train)
+
+# Define the parameter grid to search
+param_grid = {
+    'max_depth': [-1]
+}
+
+# Create the LightGBM model
+lgb_model = abc(verbose=-1,num_classes=6, objective='multiclass')
+
+# Use GridSearchCV to find the best hyperparameters
+grid_search = GridSearchCV(estimator=lgb_model, param_grid=param_grid, scoring='accuracy', cv=5)
+grid_search.fit(X_train, y_train)
+
+# Print the best hyperparameters
+print("Best Hyperparameters:", grid_search.best_params_)
+
+# Train a new model with the best hyperparameters
+best_model = abc(**grid_search.best_params_,verbose=-1, num_classes=6, objective='multiclass')
+best_model.fit(X_train, y_train)
+best_model.get_params()
+# Make predictions on the test set
+y_pred = best_model.predict(X_test)
+y_pred_train = best_model.predict(X_train)
+
+
+# Evaluate the model
+test_accuracy = accuracy_score(y_test, y_pred)
+train_accuracy = accuracy_score(y_train, y_pred_train)
+print(f'Test Accuracy with best hyperparameters: {test_accuracy}')
+print(f'Training Accuracy with best hyperparameters: {train_accuracy}')
+
