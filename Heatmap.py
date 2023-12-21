94c704ada5bc97b1e33320770c16c421bd0d0987
fardadhomafar <fardadhomafar@ymail.com>
2023-12-20 19:59:06 -0800

    1

--- Heatmap.py
+++ Heatmap.py
@@ -5,16 +5,16 @@ from matplotlib.colors import ListedColormap
 
 
 # Extract the results into a DataFrame
-results_df = pd.read_excel('LGBM_heat_n.xlsx')
+results_df = pd.read_excel('XGB_heat.xlsx')
 
 # Extract relevant columns for the heatmap
-heatmap_data = results_df.pivot(index='param_num_leaves', columns='param_feature_fraction_bynode', values='mean_test_score')
+heatmap_data = results_df.pivot(index='param_max_depth', columns='param_gamma', values='mean_test_score')
 
 custom_cmap = ListedColormap(sns.color_palette("Greens", as_cmap=True)(range(10, 256)))
 # Plot the heatmap
 plt.figure(figsize=(10, 6))
 sns.heatmap(heatmap_data, annot=True, cmap=custom_cmap, fmt=".3f", cbar_kws={'label': 'Weighted F1 Score'})
 plt.title('GridSearchCV Results Heatmap')
-plt.xlabel('feature_fraction_bynode')
-plt.ylabel('num_leaves')
+plt.xlabel('gamma')
+plt.ylabel('max_depth')
 plt.show()
